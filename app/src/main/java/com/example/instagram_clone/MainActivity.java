fbe321ebc471222ce991c31bba923c532fce3d42
Lovish Badlani <42366382+LovishB@users.noreply.github.com>
2019-12-31 16:18:49 +0530

    Second Trail

--- app/src/main/java/com/example/instagram_clone/MainActivity.java
+++ app/src/main/java/com/example/instagram_clone/MainActivity.java
@@ -4,13 +4,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
-import com.parse.SaveCallback;
+import com.parse.ParseQuery;
 import com.shashank.sony.fancytoastlib.FancyToast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +22,10 @@ public class MainActivity extends AppCompatActivity {
     private EditText name;
     private EditText email;
     private EditText pass;
+    private TextView get_name;
+    private TextView get_email;
+    private TextView get_password;
+    private String message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,27 +37,52 @@ public class MainActivity extends AppCompatActivity {
         email=findViewById(R.id.field02);
         pass=findViewById(R.id.field03);
 
-        final ParseObject Registered_Users=new ParseObject("Registered_Users");
+        get_name=findViewById(R.id.view01);
+        get_email=findViewById(R.id.view02);
+        get_password=findViewById(R.id.view03);
+        message="";
+
+        //final ParseObject Registered_Users=new ParseObject("Registered_Users");
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Registered_Users.put("Username",name.getText().toString());
-                Registered_Users.put("Email",email.getText().toString());
-                Registered_Users.put("Password",pass.getText().toString());
-                Registered_Users.saveInBackground(new SaveCallback() {
+
+                ParseQuery<ParseObject> query=ParseQuery.getQuery("Registered_Users");
+
+                query.findInBackground(new FindCallback<ParseObject>() {
                     @Override
-                    public void done(ParseException e) {
-                        if(e==null)
-                        {
-                            FancyToast.makeText(MainActivity.this,"Successfully Signed",FancyToast.LENGTH_LONG,FancyToast.SUCCESS,true).show();
+                    public void done(List<ParseObject> objects, ParseException e) {
+                        if(e==null){
+                            if(objects.size()>0){
+                                for(ParseObject parseObject : objects)
+                                {
+                                    message=message+ parseObject.get("Username") + "\n";
+                                    FancyToast.makeText(MainActivity.this,message,FancyToast.LENGTH_LONG,FancyToast.SUCCESS,true).show();
+                                }
+
+
+                            }
                         }
-                        else
+
+                    }
+                });
+
+               /* query.getInBackground("PliqsFHwMY", new GetCallback<ParseObject>() {
+                    @Override
+                    public void done(ParseObject object, ParseException e) {
+                        if(e==null && object!=null)
                         {
-                            FancyToast.makeText(MainActivity.this,"Registration Failed",FancyToast.LENGTH_LONG,FancyToast.ERROR,true).show();
+                            FancyToast.makeText(MainActivity.this,"HOLA",FancyToast.LENGTH_LONG,FancyToast.SUCCESS,true).show();
+                            get_name.setText("Username: "+object.get("Username").toString());
+                            get_password.setText("Password: "+object.get("Password").toString());
+                            get_email.setText("Email: "+object.get("Email").toString());
                         }
                     }
                 });
+
+                */
+
             }
         });
 
